84b642640d19620e5bff4353a5e1fad4e16432e2
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2024-03-23 17:57:39 -0300

    added feature component

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,10 +3,19 @@ import { Hero } from '../hero';
 import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mockheroes';
+import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
 @Component({
   selector: 'app-heroes',
   standalone: true,
-  imports: [CommonModule, FormsModule, NgFor, NgIf, UpperCasePipe, FormsModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    NgFor,
+    NgIf,
+    UpperCasePipe,
+    FormsModule,
+    HeroDetailComponent,
+  ],
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
@@ -15,6 +24,6 @@ export class HeroesComponent {
   selectedHero: Hero | undefined;
   onSelect(hero: Hero) {
     this.selectedHero = hero;
-    console.log('Clicked!!', hero.name)
+    console.log('Clicked!!', hero.name);
   }
 }
