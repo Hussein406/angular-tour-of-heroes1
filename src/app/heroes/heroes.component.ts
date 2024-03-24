51ab7ff5b9f6ead3fd96acd53643c5f9ce4d7af9
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2024-03-23 22:22:00 -0300

    toh part four

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,9 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { CommonModule, NgFor, NgIf, UpperCasePipe } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { HEROES } from '../mockheroes';
 import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../hero.service';
 @Component({
   selector: 'app-heroes',
   standalone: true,
@@ -19,11 +20,22 @@ import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
   templateUrl: './heroes.component.html',
   styleUrl: './heroes.component.css',
 })
-export class HeroesComponent {
-  heroes = HEROES;
-  selectedHero: Hero | undefined;
+export class HeroesComponent implements OnInit {
+  selectedHero?: Hero;
+
+  heroes: Hero[] = [];
+
+  constructor(private heroService: HeroService) {}
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero) {
     this.selectedHero = hero;
     console.log('Clicked!!', hero.name);
+  }
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
   }
 }
