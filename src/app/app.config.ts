95e57a8c32541420f9088f4b676a876cda561d4d
Abdillahi Hussein <abdillahi.hussein@ibm.com>
2024-03-25 07:53:07 -0300

    Major code added

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,19 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient } from '@angular/common/http';
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService } from './in-memory-data.service';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()],
+  providers: [
+    provideHttpClient(),
+    importProvidersFrom(
+      InMemoryWebApiModule.forRoot(InMemoryDataService, { delay: 1000 })
+    ),
+    provideRouter(routes),
+    provideClientHydration(),
+  ],
 };
